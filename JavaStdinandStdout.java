7f09dc62958c6f6ad593588668611e532dc85e4f
Giovanni Emmanuel Arevalo Perez <giovanni.arevalo@telusinternational.com>
2022-03-11 17:34:28 -0600

    java challenges 1,2,3 level:easy

---
+++ JavaStdinandStdout.java
@@ -0,0 +1,33 @@
+/*
+Task
+In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format
+There are  lines of input, and each line contains a single integer.
+
+Sample Input
+
+42
+100
+125
+
+Sample Output
+
+42
+100
+125
+
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);    
+        while(scan.hasNext()){
+             int a = scan.nextInt();
+             System.out.println(a);
+        }
+    }
+}
